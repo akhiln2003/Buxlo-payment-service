3b4eeddccc537c7efc4e2d838f85f967a03002a4
akhil <4khiln@gmail.com>
2025-07-30 20:17:47 +0530

    chore(payment): remove console.log from subscription plan use case
    - Cleaned up development logs in
    - Minor Dockerfile adjustments for consistency

--- src/application/usecase/admin/updateSubscriptionPlanUseCase.ts
+++ src/application/usecase/admin/updateSubscriptionPlanUseCase.ts
@@ -15,7 +15,6 @@ export class UpdateSubscriptionPlanUseCase
     updatedData: IsubscriptionPlanData
   ): Promise<any | Subscription> {
     try {
-      console.log(id,updatedData);
       
       const data = await this.subscriptionPlanRepository.update(
         id,
